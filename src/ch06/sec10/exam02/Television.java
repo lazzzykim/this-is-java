df57762c2bf4a20f934b8dea55cb64417d0ab619
lazzzykim <33sunghyeon@gmail.com>
2024-06-23 22:53:20 +0900

    클래스

---
+++ src/ch06/sec10/exam02/Television.java
@@ -0,0 +1,12 @@
+package ch06.sec10.exam02;
+
+public class Television {
+
+    static String company = "SAMSUNG";
+    static String model = "OLED";
+    static String info;
+
+    static {
+        info = company + "-" + model;
+    }
+}
---
+++ src/ch06/sec11/KoreanEx.java
@@ -0,0 +1,13 @@
+package ch06.sec11;
+
+public class KoreanEx {
+    public static void main(String[] args) {
+
+        Korean k1 = new Korean("941128-1231234", "김성현");
+
+        System.out.println(k1.nation);
+        System.out.println(k1.name);
+        System.out.println(k1.ssn);
+
+    }
+}

5f603461342d6ed1b0dfc8b8bbec9dc5419088a7
lazzzykim <33sunghyeon@gmail.com>
2024-07-04 23:51:44 +0900

    인터페이스 다형성

---
+++ src/ch08/sec11/Car.java
@@ -0,0 +1,12 @@
+package ch08.sec11;
+
+public class Car {
+
+    Tire tire1 = new HanKookTire();
+    Tire tire2 = new HanKookTire();
+
+    void run() {
+        tire1.roll();
+        tire2.roll();
+    }
+}

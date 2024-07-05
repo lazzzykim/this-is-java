3e27f50cbd39fa296986965c35b5b9f541ce4e90
lazzzykim <33sunghyeon@gmail.com>
2024-07-06 01:07:04 +0900

    중첩 인터페이스

---
+++ src/ch09/sec06/ex02/ButtonEx.java
@@ -0,0 +1,34 @@
+package ch09.sec06.ex02;
+
+public class ButtonEx {
+    public static void main(String[] args) {
+
+        Button btnOk = new Button();
+
+        // OK 버튼 클릭 이벤트를 처리할 ClickListener 구현 클래스
+        class OkListener implements Button.ClickListener {
+            @Override
+            public void onClick() {
+                System.out.println("ok 버튼을 클릭했습니다.");
+            }
+        }
+
+        // Ok 버튼 객체에 ClickListener 구현 객체 주입
+        btnOk.setClickListener(new OkListener());
+
+        btnOk.click();
+
+        Button btnCancel = new Button();
+
+        class CancelListener implements Button.ClickListener {
+            @Override
+            public void onClick() {
+                System.out.println("cancel button 클릭");
+            }
+        }
+
+        btnCancel.setClickListener(new CancelListener());
+
+        btnCancel.click();
+    }
+}
